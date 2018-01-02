233fdfd6a541faf886a97414baf960849b0aae68
Blake Regalia <blake.regalia@gmail.com>
2018-01-01 19:03:29 -0800

    fixed name

--- module.js
+++ module.js
@@ -2,7 +2,7 @@ const cp = require('child_process');
 
 const mmap = require('mmap-io');
 
-const shm = require('./build/Release/posix_shm');
+const shm = require('./build/Release/shmmap');
 
 let s_prefix = '/lw';
 let i_key = Math.floor(Math.random()*1e12);
